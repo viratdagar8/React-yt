60a7bea990d34ba2c15e164946e4b46f563d185c
Virat Dagar <dagarvirat8@gmail.com>
2026-08-05 14:40:47 +0530

    initial commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1 id="jaat">JAAT</h1>
+      <h1 id="welcome">Welcome to JAATLAND</h1>
+    <div/>
+    
+    <div id="container">
+      love u
+
+    </div> 
+
+    </>
+   )
+}
+
+export default App
