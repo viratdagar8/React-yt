b1849ce230158fc5a08f0855542307408dc663ac
Virat Dagar <dagarvirat8@gmail.com>
2026-08-09 00:04:00 +0530

    final commit

--- 03_props/src/App.jsx
+++ 03_props/src/App.jsx
@@ -4,9 +4,9 @@ import Card from './components/Card'
 const App = () => {
   return (
     <div className='parent'>
-      <Card user='Virat' age={20} />
-      <Card user='samrat' age={19} />
-
+      <Card user='Virat' age={20} img="https://plus.unsplash.com/premium_photo-1733317257106-79b094845b8e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGFic3RyYWN0JTIwZmFjZXxlbnwwfHwwfHx8MA%3D%3D" />
+      <Card user='Samrat' age={19} img="https://plus.unsplash.com/premium_photo-1711987698775-aedfc9fa658e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YWJzdHJhY3QlMjBmYWNlfGVufDB8fDB8fHww" />
+      <Card user='Arnav' age={12} img="https://plus.unsplash.com/premium_photo-1686546766850-f857e964814f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8YWJzdHJhY3QlMjBmYWNlfGVufDB8fDB8fHww" />
     </div>
   )
 }
--- 03_props/src/components/card.jsx
+++ 03_props/src/components/card.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
 
-const card = (props) => {
+const Card = (props) => {
 
   console.log(props);
  
   return (
     <div className="card">
-      <img src="https://images.unsplash.com/photo-1768387760576-be05730e90c2?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D" alt="Card Image" />
-      <h1>{props.user}</h1>
-      <p>Card content goes here.</p>
+      <img src={props.img} alt="" />
+      <h1>{props.user}, {props.age}</h1>
+     
       <button>View Profile</button>
     </div>
   )
 }
 
-export default card
+export default Card
