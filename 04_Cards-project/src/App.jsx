ef7648c3d519d8dba5c90dcde6a32e72bec6993e
Virat Dagar <dagarvirat8@gmail.com>
2026-08-12 00:05:50 +0530

    modifocation

---
+++ 04_Cards-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Bookmark} from 'lucide-react'
+import Card from './components/Card'
+
+const App = () => {
+  return (
+    <div className='parent'>
+      <Card />
+
+    </div>
+)
+}
+
+export default App 
